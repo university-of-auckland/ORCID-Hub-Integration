9985baa9ee431545250bb0f501c31c2edc2d5749
Rad Cirskis <nad2000@gmail.com>
2019-07-12 20:01:26 +1200

    so far ...works smoothly

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# consumer
+# Consumer simple message agnostic event consumer
+
+a sample Lambda...
+It can be triggered either by SQS or API Gateway directly.
+
+# Building
+
+```
+go build main.go && upx main && zip main.zip main main.go
+```
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+)
+
+type Platform interface {
+}
+
+// Generic message suitable for both EMP Update event and
+// ORCIDHub Webhook propagagted event:
+type Event struct {
+	EPPN    string `json:"eppn"`
+	Email   string `json:"email"`
+	Name    string `json:"name"`
+	ORCID   string `json:"orcid"`
+	Subject int    `json:"subject"`
+	Type    string `json:"type"`
+	URL     string `json:"url"`
+	// SQS Message if used SQS
+	Records []events.SQSMessage
+}
+
+func processEmpUpdate(e Event) (string, error) {
+	return "", nil
+}
+
+func processUserRegistration(e Event) (string, error) {
+	return "", nil
+}
+
+func HandleRequest(ctx context.Context, e Event) (string, error) {
+	log.Printf("Cotext: %#v", ctx)
+	lc, _ := lambdacontext.FromContext(ctx)
+	log.Printf("Lambda Context: %#v", lc)
+	log.Print(lc.Identity.CognitoIdentityPoolID)
+	for _, pair := range os.Environ() {
+		log.Println(pair)
+	}
+	log.Printf("Recieved: %#v", e)
+	if e.Records != nil {
+		for _, r := range e.Records {
+			var e Event
+			json.Unmarshal([]byte(r.Body), &e)
+			log.Printf("Event: %#v", e)
+		}
+	}
+
+	return fmt.Sprintf("Recieved: %v", e), nil
+
+	// var e Event
+	// err := json.Unmarshal(message, &e)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if e.Subject != 0 {
+	// 	processEmpUpdate(e)
+	// } else if e.Type == "CREATED" {
+	// 	processUserRegistration(e)
+	// } else {
+	// 	log.Printf("The event %#v discarded.", e)
+	// }
+	// return fmt.Sprintf("Recieved: %#v", e), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
