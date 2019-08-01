36a5f7e97f614bb0fdcfb97b4696775d7c9e69fe
Rad Cirskis <nad2000@gmail.com>
2019-08-01 13:29:48 +1200

    removed unused fields

--- handler/identity.go
+++ handler/identity.go
@@ -7,49 +7,6 @@ import (
 
 // Identity - the user identity record.
 type Identity struct {
-	Addresses []struct {
-		CareOf      string `json:"careOf"`
-		City        string `json:"city"`
-		Country     string `json:"country"`
-		CountryID   string `json:"countryId"`
-		Dpid        string `json:"dpid"`
-		LastUpdated string `json:"lastUpdated"`
-		Line1       string `json:"line1"`
-		Line2       string `json:"line2"`
-		Line3       string `json:"line3"`
-		Postcode    string `json:"postcode"`
-		State       string `json:"state"`
-		StateID     string `json:"stateId"`
-		Suburb      string `json:"suburb"`
-		Type        string `json:"type"`
-		TypeID      string `json:"typeId"`
-	} `json:"addresses"`
-	Citizenships []struct {
-		Country   string `json:"country"`
-		CountryID string `json:"countryId"`
-	} `json:"citizenships"`
-	CountryOfBirth struct {
-		Country   string `json:"country"`
-		CountryID string `json:"countryId"`
-	} `json:"countryOfBirth"`
-	Deceased struct {
-		Comments         string `json:"comments"`
-		Date             string `json:"date"`
-		Dead             bool   `json:"dead"`
-		DeathCertificate string `json:"deathCertificate"`
-		Place            string `json:"place"`
-	} `json:"deceased"`
-	DisabilityInfo struct {
-		Disabilities []struct {
-			ID   string `json:"id"`
-			Type string `json:"type"`
-		} `json:"disabilities"`
-		DisabilityPermanent bool `json:"disabilityPermanent"`
-		DisabilitySupport   bool `json:"disabilitySupport"`
-		IsDisabled          bool `json:"isDisabled"`
-	} `json:"disabilityInfo"`
-	DisplayName  string `json:"displayName"`
-	Dob          string `json:"dob"`
 	EmailAddress string `json:"emailAddress"`
 	Emails       []struct {
 		Email       string `json:"email"`
@@ -58,65 +15,12 @@ type Identity struct {
 		TypeID      string `json:"typeId"`
 		Verified    bool   `json:"verified"`
 	} `json:"emails"`
-	EmergencyContacts []struct {
-		EmailAddress string `json:"emailAddress"`
-		LastUpdated  string `json:"lastUpdated"`
-		Name         string `json:"name"`
-		Phones       []struct {
-			AreaCode    string `json:"areaCode"`
-			CountryCode string `json:"countryCode"`
-			Extension   string `json:"extension"`
-			LastUpdated string `json:"lastUpdated"`
-			Number      string `json:"number"`
-			Type        string `json:"type"`
-			TypeID      string `json:"typeId"`
-		} `json:"phones"`
-		Relationship string `json:"relationship"`
-	} `json:"emergencyContacts"`
-	Ethnicities []struct {
-		ID    string `json:"id"`
-		Value string `json:"value"`
-	} `json:"ethnicities"`
 	ExtIds []struct {
 		ID   string `json:"id"`
 		Type string `json:"type"`
 	} `json:"extIds"`
-	FirstName       string   `json:"firstName"`
-	Gender          string   `json:"gender"`
-	Groups          []string `json:"groups"`
-	ID              int      `json:"id"`
-	IDPhotoExists   bool     `json:"idPhotoExists"`
-	IwiAffiliations []struct {
-		ID    string `json:"id"`
-		Value string `json:"value"`
-	} `json:"iwiAffiliations"`
-	LastName   string `json:"lastName"`
-	MergedToID string `json:"mergedToId"`
-	Mobile     string `json:"mobile"`
-	Names      []struct {
-		First       string `json:"first"`
-		Last        string `json:"last"`
-		LastUpdated string `json:"lastUpdated"`
-		Middle      string `json:"middle"`
-		Suffix      string `json:"suffix"`
-		Title       string `json:"title"`
-		Type        string `json:"type"`
-	} `json:"names"`
-	Phones []struct {
-		AreaCode    string `json:"areaCode"`
-		CountryCode string `json:"countryCode"`
-		Extension   string `json:"extension"`
-		LastUpdated string `json:"lastUpdated"`
-		Number      string `json:"number"`
-		Type        string `json:"type"`
-		TypeID      string `json:"typeId"`
-	} `json:"phones"`
-	PreviousIds     []string `json:"previousIds"`
-	PrimaryIdentity bool     `json:"primaryIdentity"`
-	Residency       string   `json:"residency"`
-	Resolved        bool     `json:"resolved"`
-	Upi             string   `json:"upi"`
-	WhenUpdated     string   `json:"whenUpdated"`
+	ID  int    `json:"id"`
+	Upi string `json:"upi"`
 }
 
 // GetORCID returns the principal part of ORCID iD
