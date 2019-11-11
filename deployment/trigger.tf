154df8fdf3421e9960a5fbdef413659cd102cfda
Rad Cirskis <nad2000@gmail.com>
2019-11-11 18:40:44 +1300

    [PROVISION][RECREATE]

--- deployment/trigger.tf
+++ deployment/trigger.tf
@@ -1,23 +1,23 @@
-resource "null_resource" "trigger" {
-  depends_on = [aws_api_gateway_deployment.ORCIDHUB_INTEGRATION_API_deployment]
-  provisioner "local-exec" {
-    command = "./create.sh"
-    environment = {
-			APIKEY = "${local.APIKEY}"
-			ENV = "${local.ENV}"
-			CLIENT_ID = "${local.CLIENT_ID}"
-			CLIENT_SECRET = "${local.CLIENT_SECRET}"
-			UPSTREAM_URL = "${local.UPSTREAM_URL}"
-    }
-	}
-  provisioner "local-exec" {
-		when    = "destroy"
-    command = "./destroy.sh"
-    environment = {
-			APIKEY = "${local.APIKEY}"
-			ENV = "${local.ENV}"
-			CLIENT_ID = "${local.CLIENT_ID}"
-			CLIENT_SECRET = "${local.CLIENT_SECRET}"
-    }
-	}
-}
+# resource "null_resource" "trigger" {
+#   depends_on = [aws_api_gateway_deployment.ORCIDHUB_INTEGRATION_API_deployment]
+#   provisioner "local-exec" {
+#     command = "./create.sh"
+#     environment = {
+# 			APIKEY = "${local.APIKEY}"
+# 			ENV = "${local.ENV}"
+# 			CLIENT_ID = "${local.CLIENT_ID}"
+# 			CLIENT_SECRET = "${local.CLIENT_SECRET}"
+# 			UPSTREAM_URL = "${local.UPSTREAM_URL}"
+#     }
+# 	}
+#   provisioner "local-exec" {
+# 		when    = "destroy"
+#     command = "./destroy.sh"
+#     environment = {
+# 			APIKEY = "${local.APIKEY}"
+# 			ENV = "${local.ENV}"
+# 			CLIENT_ID = "${local.CLIENT_ID}"
+# 			CLIENT_SECRET = "${local.CLIENT_SECRET}"
+#     }
+# 	}
+# }
