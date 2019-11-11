87bf11e040a40c59f0aaa3840ba372dc6ebfe585
Rad Cirskis <nad2000@gmail.com>
2019-11-11 14:02:39 +1300

    [PROVISION][RECREATE]

--- deployment/purge.sh
+++ deployment/purge.sh
@@ -1,22 +1,24 @@
+#!/use/bin/env bash
 # Use this to remove role and dependnat objects terraform fails to deal
 # when you run into 'EntityAlreadyExists'
+
 if [ "$ENV" != "prod" ] && [ "$ENV" != "default" ] ; then
   SUFFIX=_$ENV
 fi
 
 for id in $(aws apigateway get-rest-apis | jq -r ".items|.[]|select(.name == \"ORCIDHUB_INTEGRATION_API_Terraform${SUFFIX}\")|.id") ; do
-  aws apigateway delete-rest-api --rest-api-id $id
+  aws apigateway delete-rest-api --rest-api-id "$id"
 done
 
 for id in $(aws apigateway get-rest-apis | jq -r ".items|.[]|select(.name == \"ORCIDHUB_INTEGRATION_API${SUFFIX}\")|.id") ; do
-  aws apigateway delete-rest-api --rest-api-id $id
+  aws apigateway delete-rest-api --rest-api-id "$id"
 done
 
 aws lambda delete-function --function-name ORCIDHUB_INTEGRATION$SUFFIX
 
 ROLE=ORCIDHUB_INTEGRATION_API_role$SUFFIX
 ARN=$(aws iam list-attached-role-policies --role-name $ROLE | jq -r ".AttachedPolicies|.[]|.PolicyArn")
-aws iam detach-role-policy  --role-name $ROLE --policy-arn $ARN
+aws iam detach-role-policy  --role-name "$ROLE" --policy-arn "$ARN"
 
 # ARN=$(aws iam list-policies --only-attached | jq -r ".Policies|.[]|select(.PolicyName == \"ORCIDHUB_INTEGRATION_API_policy$SUFFIX\")|.Arn")
 # ARN=$(aws iam list-policies --path-prefix /ORCIDHUB/INTEGRATION/ | jq -r ".Policies|.[]|select(.PolicyName == \"ORCIDHUB_INTEGRATION_API_policy$SUFFIX\")|.Arn")
@@ -24,7 +26,7 @@ aws iam detach-role-policy  --role-name $ROLE --policy-arn $ARN
 # aws iam list-attached-role-policies --role-name ORCIDHUB_INTEGRATION_API_role$SUFFIX
 
 for id in $(aws iam list-policies | jq -r ".Policies|.[]|select(.PolicyName|contains(\"ORCIDHUB_INTEGRATION_API_policy$SUFFIX\"))|.Arn") ; do
-  aws iam delete-policy --policy-arn $id
+  aws iam delete-policy --policy-arn "$id"
 done
 
 aws iam delete-role --role-name $ROLE
