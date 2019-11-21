980bd3b47bc7423c2bde603f681ef7749c44afaa
Rad Cirskis <nad2000@gmail.com>
2019-11-21 17:03:17 +1300

    ...

--- .jenkins/install.sh
+++ .jenkins/install.sh
@@ -35,18 +35,3 @@ fi
 # if [ ! -x "${WORKSPACE}/bin/zipit" ] ; then 
 #   go build -o "${WORKSPACE}/bin/zipit" .jenkins/zipit.go
 # fi
-
-# recompile terraform 'null' provider:
-if [ ! -x "${GOPATH}/bin/terraform-provider-null" ] ; then
-  # git clone --depth 1 https://github.com/terraform-providers/terraform-provider-null.git "$GOPATH/src/github.com/terraform-providers/terraform-provider-null"
-  # cd "$GOPATH/src/github.com/terraform-providers/terraform-provider-null"
-
-  wget -q https://github.com/terraform-providers/terraform-provider-null/archive/master.zip -O null.zip
-  mkdir -p "$GOPATH/src/github.com/terraform-providers/"
-  unzip -q null.zip -d "$GOPATH/src/github.com/terraform-providers/"
-  cd "$GOPATH/src/github.com/terraform-providers/"
-  mv terraform-provider-null-master terraform-provider-null
-  cd terraform-provider-null
-  go install
-
-fi 
---
+++ deployment/patch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# patch terraform: override the null provider
+set -xe
+
+# recompile terraform 'null' provider:
+if [ ! -x "${GOPATH}/bin/terraform-provider-null" ] ; then
+  # git clone --depth 1 https://github.com/terraform-providers/terraform-provider-null.git "$GOPATH/src/github.com/terraform-providers/terraform-provider-null"
+  # cd "$GOPATH/src/github.com/terraform-providers/terraform-provider-null"
+
+  wget -q https://github.com/terraform-providers/terraform-provider-null/archive/master.zip -O null.zip
+  mkdir -p "$GOPATH/src/github.com/terraform-providers/"
+  unzip -q null.zip -d "$GOPATH/src/github.com/terraform-providers/"
+  cd "$GOPATH/src/github.com/terraform-providers/"
+  mv terraform-provider-null-master terraform-provider-null
+  cd terraform-provider-null
+  go install
+
+fi 
+
+cp -T $GOPATH/bin/terraform-provider-null .terraform/plugins/linux_amd64/terraform-provider-null_*
